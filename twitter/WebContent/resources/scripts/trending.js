38834dd676181ab0140ff0dc1e3c86fb8b158a48
TiTo <tito.suarez.b@gmail.com>
2016-09-29 00:41:06 -0500

    Almost done

---
+++ twitter/WebContent/resources/scripts/trending.js
@@ -0,0 +1,134 @@
+(function() {
+
+	$(document).ready(fnInit);
+
+	function fnInit() {
+
+		var count = 1;
+
+		$(window).scroll(
+				function() {
+					if ($(window).scrollTop() == $(document).height()
+							- $(window).height()) {
+						count++;
+						getTweets(count);
+					}
+				});
+
+		fnGetTrending();
+
+		$("#selTrending").change(function() {
+			count = 1;
+			getTweets(count);
+		});
+
+	}
+
+	function fnGetTrending() {
+
+		$.ajax({
+			url : 'TwitterTrends',
+			dataType : 'json',
+			cache : false,
+			success : function(objResponse) {
+
+				$(objResponse).each(
+						function(index, item) {
+							$("#selTrending").append(
+									$("<option>").val(item.name)
+											.text(item.name));
+						});
+
+			}
+		});
+
+	}
+
+	function getTweets(intPage) {
+
+		if (intPage == 1) {
+			$("#twits-container").html("");
+		}
+
+		var strTrend = $("#selTrending").val();
+
+		if (strTrend == false) {
+			return;
+		}
+
+		var arrData = {
+			trend : strTrend,
+			page : intPage
+		};
+
+		$.ajax({
+			url : 'TweetsByTrend',
+			type : 'POST',
+			data : arrData,
+			dataType : 'json',
+			cache : false,
+			success : function(objResponse) {
+
+				$(objResponse).each(function(index, item) {
+					$("#twits-container").append(fnDrawTweet(item));
+				});
+
+			}
+		});
+
+	}
+
+	function fnDrawTweet(item) {
+
+		htmlTwit = "<div class='col-xs-12 col-sm-6 col-md-4'>";
+
+		htmlTwit += "<div class='tweet' " + "data-url=" + item.url + "' >";
+
+		htmlTwit += "<div class='profile_pic' > <a href='" + item.url
+				+ "' target='_blank' ><img src='" + item.profile_pic
+				+ "' alt='' /></a></div>";
+
+		htmlTwit += "<div class='name'>" + item.name + "</div>";
+		htmlTwit += "<div class='tweet_content'> <a href='" + item.url
+				+ "' target='_blank' >" + item.tweet_content + "</a></div>";
+		htmlTwit += "<div class='location'>" + item.location + "</div>";
+		htmlTwit += "</div>";
+		htmlTwit += "</div>";
+
+		return htmlTwit;
+	}
+
+	jQuery(document)
+			.ready(
+					function($) {
+
+						function loadArticle(pageNumber) {
+							$('a#inifiniteLoader').show('fast');
+							$
+									.ajax({
+										url : "<?php bloginfo('wpurl') ?>/wp-admin/admin-ajax.php",
+										type : 'POST',
+										data : "action=infinite_scroll&page_no="
+												+ pageNumber
+												+ '&loop_file=loop',
+										success : function(html) {
+											$('a#inifiniteLoader').hide('1000');
+											$("#content").append(html); // This
+											// will
+											// be
+											// the
+											// div
+											// where
+											// our
+											// content
+											// will
+											// be
+											// loaded
+										}
+									});
+							return false;
+						}
+
+					});
+
+})();
---
+++ twitter/src/com/twitter/controller/Trends.java
@@ -0,0 +1,39 @@
+package com.twitter.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.twitter.TwitterClass;
+
+import twitter4j.TwitterException;
+
+@WebServlet("/TwitterTrends")
+public class Trends extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private TwitterClass objTwiter;
+
+	public void init() {
+		objTwiter = new TwitterClass();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+
+		String jsonResponse = "";
+
+		try {
+			jsonResponse = objTwiter.Trending();
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
+
+		response.getWriter().println(jsonResponse);
+	}
+
+}
